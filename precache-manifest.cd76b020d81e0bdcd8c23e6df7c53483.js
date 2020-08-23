c3417302a8a1b1b31f97441561b9d00647377db7
Bruno Silva <bruno3dcontato@gmail.com>
2020-08-23 16:46:04 -0300

    Updates

---
+++ precache-manifest.cd76b020d81e0bdcd8c23e6df7c53483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d5bab74bae47f92e3544a1efc677bd",
+    "url": "/eval-js-benchmark/index.html"
+  },
+  {
+    "revision": "05f90641acaff6b9894c",
+    "url": "/eval-js-benchmark/static/css/main.bd279d76.chunk.css"
+  },
+  {
+    "revision": "36361dc74717849273e9",
+    "url": "/eval-js-benchmark/static/js/2.8a88a306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eval-js-benchmark/static/js/2.8a88a306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f90641acaff6b9894c",
+    "url": "/eval-js-benchmark/static/js/main.0028bbd8.chunk.js"
+  },
+  {
+    "revision": "eb996b56ba7f15e0eb7c",
+    "url": "/eval-js-benchmark/static/js/runtime-main.602a697c.js"
+  }
+]);
